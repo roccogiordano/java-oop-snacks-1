7bb1227881ea06fd7c484afbb8cd3dd6c2fe7b93
roccogiordano <rocco.giordano573@gmail.com>
2024-12-02 17:32:20 +0100

    feat: added Student class methods

--- java-oop-snacks-1/src/Student.java
+++ java-oop-snacks-1/src/Student.java
@@ -4,10 +4,29 @@ public class Student {
 
     // Instance Variables Declaration
 
-    String firstName;
-    String lastName;
-    int age;
+    private String firstName;
+    private String lastName;
+    private int age;
 
+
+    //
+
+
+    // Student methods
+
+    public Student(String firstName, String lastName, int age) {
+
+        this.firstName = firstName;
+        
+        this.lastName = lastName;
+
+        this.age = age;
+
+    }
+
+    public String getInfo() {
+        return (firstName + " " + lastName + ", " + age + " anni.");
+    }
 
     //
 

f7fd91bc44f085fdd37ccb8dbbd61e0dd61fb440
roccogiordano <rocco.giordano573@gmail.com>
2024-12-02 17:48:44 +0100

    feat: added BankAccount class methods

--- java-oop-snacks-1/src/BankAccount.java
+++ java-oop-snacks-1/src/BankAccount.java
@@ -7,6 +7,39 @@ public class BankAccount {
     private int customerReference;
     private int balance;
 
+
+    //
+
+
+    // BankAccount Methods
+
+    public BankAccount(int customerReference) {
+
+        this.customerReference = customerReference;
+
+        this.balance = 0;
+
+    }
+
+    public void deposit(int depositAmount) {
+
+        this.balance = balance + depositAmount;
+
+    }
+
+    public void withdrawal(int withdrawalAmount) {
+
+        this.balance = balance - withdrawalAmount;
+
+    }
+
+    public int getBalance() {
+
+        return balance;
+
+    }
+
+
     //
 
 }

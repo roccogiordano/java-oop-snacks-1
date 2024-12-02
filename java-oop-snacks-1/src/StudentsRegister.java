3d03458dbeb9b04009674551296dcc479a286925
roccogiordano <rocco.giordano573@gmail.com>
2024-12-03 00:25:18 +0100

    feat: added StudentsRegister class woth instance variables

---
+++ java-oop-snacks-1/src/StudentsRegister.java
@@ -0,0 +1,17 @@
+// Class Definition
+
+public class StudentsRegister {
+
+    // Instance Variables Declaration
+
+    private Object[] enrolledStudents;
+
+    private int maxStudents = 25;
+
+    private int currentStudents = 0;
+
+    //
+
+}
+
+//

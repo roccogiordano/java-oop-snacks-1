486cca0b0b06956c92da66431fa190422d9ebc1a
roccogiordano <rocco.giordano573@gmail.com>
2024-12-02 17:37:30 +0100

    feat: added Main with Student instances and methods

---
+++ java-oop-snacks-1/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Student Instances
+        
+        Student studente1 = new Student("John", "Smith", 24);
+
+        Student studente2 = new Student("Mark", "Rober", 38);
+
+        Student studente3 = new Student("William", "Park", 19);
+
+        //
+
+
+        // Student Methods Call
+
+        System.out.println(studente1.getInfo());
+
+        System.out.println(studente2.getInfo());
+
+        System.out.println(studente3.getInfo());
+
+        //
+
+    }
+}
